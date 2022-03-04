04e0c55320fa9b6aca9ebecc963399a6b93a05af
Valentin Drenkov <drenkovvv@gmail.com>
2022-03-04 15:46:13 +0200

    Version DB

--- Cloc/Database/DBConnection.cs
+++ Cloc/Database/DBConnection.cs
@@ -1,48 +1,48 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using MySql.Data.MySqlClient;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Cloc.Database
-{
-    public class DBConnection
-    {
-        public string Server { get; set; }
-        public string DatabaseName { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+//namespace Cloc.Database
+//{
+//    public class DBConnection
+//    {
+//        public string Server { get; set; }
+//        public string DatabaseName { get; set; }
+//        public string UserName { get; set; }
+//        public string Password { get; set; }
 
-        public MySqlConnection Connection { get; set; }
+//        public MySqlConnection Connection { get; set; }
 
-        private static DBConnection _instance = null;
-        public static DBConnection Instance()
-        {
-            if (_instance == null)
-                _instance = new DBConnection();
-            return _instance;
-        }
+//        private static DBConnection _instance = null;
+//        public static DBConnection Instance()
+//        {
+//            if (_instance == null)
+//                _instance = new DBConnection();
+//            return _instance;
+//        }
 
-        public bool IsConnect()
-        {
-            if (Connection == null)
-            {
-                if (String.IsNullOrEmpty(DatabaseName))
-                    return false;
-                string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
-                Connection = new MySqlConnection(connstring);
-                Connection.Open();
-            }
+//        public bool IsConnect()
+//        {
+//            if (Connection == null)
+//            {
+//                if (String.IsNullOrEmpty(DatabaseName))
+//                    return false;
+//                string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+//                Connection = new MySqlConnection(connstring);
+//                Connection.Open();
+//            }
 
-            return true;
-        }
+//            return true;
+//        }
 
-        public void Close()
-        {
-            Connection.Close();
-        }
+//        public void Close()
+//        {
+//            Connection.Close();
+//        }
 
 
-    }
-}
+//    }
+//}
